ad8b7a61e51769bead87c46fffbf5cbb6bfa5ebf
sojeongw <redthink90@gmail.com>
2020-06-04 14:20:52 +0900

    더 자바-애플리케이션을 테스트하는 다양한 방법: JUnit 시작하기

---
+++ src/main/java/me/dodeon/inflearnthejavatest/Study.java
@@ -0,0 +1,4 @@
+package me.dodeon.inflearnthejavatest;
+
+public class Study {
+}
---
+++ src/test/java/me/dodeon/inflearnthejavatest/StudyTest.java
@@ -0,0 +1,46 @@
+package me.dodeon.inflearnthejavatest;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudyTest {
+
+    // JUnit 5부터는 public을 붙이지 않아도 된다. 리플렉션을 사용해서 private 한 메서드에도 접근 가능하므로 굳이 public일 필요가 없기 때문이다.
+    @Test
+    void a() {
+        Study study = new Study();
+        assertNotNull(study);
+        System.out.println("a");
+    }
+
+    @Test
+    @Disabled
+    void b() {
+        System.out.println("b");
+    }
+
+    // 테스트 클래스 안에 있는 모든 테스트가 실행될 때 딱 한 번만 호출된다.
+    // private이 아니면서 static이고 리턴값이 없는 메서드만 사용해야 한다. 메서드 이름은 상관없다.
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    // 각각의 테스트를 실행하기 이전, 이후에 실행된다.
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("after each");
+    }
+
+}
